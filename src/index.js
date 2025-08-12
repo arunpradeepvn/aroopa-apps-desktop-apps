ee6fd4709fe18d5fce8f4d3ab47604f2152041b9
Arun <Arunpradeep.Nagarajan@aroopatech.com>
2025-08-12 16:31:15 +0530

    update: url

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function createWindow() {
 
   // win.webContents.openDevTools();
 
-  win.loadURL("https://app.molwayecosystem.com/");
+  win.loadURL("https://aroopa.aroopaapps.com");
 }
 
 app.whenReady().then(() => {
